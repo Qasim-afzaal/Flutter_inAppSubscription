14de5e2ead4a5176f2bda4f8b879baa5561e5c90
Qasim-afzaal <qaim.afzaal432@gmail.com>
2024-10-01 03:32:59 +0500

    git test

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(child: Text(""));
   }
 }
