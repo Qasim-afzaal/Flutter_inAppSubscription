2df14ee14477bc65a0476834b5e4e6dd3fafbd1c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-04 02:23:54 +0500

    web plugin register

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,42 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:connectivity_plus_web/connectivity_plus_web.dart';
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:file_picker/_internal/file_picker_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:image_cropper_for_web/image_cropper_for_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:smart_auth/smart_auth_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  ConnectivityPlusPlugin.registerWith(registrar);
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FilePickerWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  ImageCropperPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
+  SmartAuthWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
