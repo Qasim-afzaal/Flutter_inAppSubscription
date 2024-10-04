f5cd48553d3176423f5e25114f04244474ed864e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-05 00:38:46 +0500

    integrate app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFB5FD21);
+  static const Color blue = Color(0xFF1787E7);
+  static const Color yellowLight = Color(0xFFF6CE42);
+
+  static const Color textGrey = Color(0xFF434343);
+
+  static const Color white = Color(0xFFffffff);
+  static const Color black = Color(0xFF000000);
+  static const Color red = Color(0xFFFF0000);
+  static const Color gry = Color(0xFF8C93A3);
+  static const Color lightBlack = Color(0xff161616);
+  static const Color aqua = Color(0xff26DBDC);
+  static const Color deepSkyBlue = Color(0xff1787E7);
+
+  static const Color transparent = Colors.transparent;
+  static const Color masterGradiantOne = Color(0xffBD7B26);
+  static const Color masterGradiantTwo = Color(0xffD19A3F);
+  static const Color backgroundColor = Color(0xffFDF9F2);
+  static const Color lightMasterThree = Color(0xffF2D8A8);
+  static const Color lightMasterTwo = Color(0xffD19A3F);
+  static const Color lightMasterOne = Color(0xffBD7B26);
+  static const Color masterColor = Color(0xff9F641D);
+  static const Color greenGrey = Color(0xff9EAEA6);
+  static const Color greenGradiantOne = Color(0xff4F5E27);
+  static const Color greenGradiantTwo = Color(0xff6A7B39);
+  static const Color stockColor = Color(0xffD0D0D0);
+  static const Color greyColor = Color(0xff939393);
+  static const Color lightBlack1 = Color(0xff2B2A29);
+  static const Color placeHolderColor = Color(0xff708379);
+  static const Color indicatorColor = Color(0xffFDF9F2);
+  static const Color indicatorForegroundColor = masterGradiantOne;
+}
