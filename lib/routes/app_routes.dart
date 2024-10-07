16f8eee2d08388a580cae76bee890ccfd05f2b95
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-08 04:30:51 +0500

    add app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,72 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const SPLASH = _Paths.SPLASH;
+  static const ON_BOARDING = _Paths.ON_BOARDING;
+  static const PROFILE_PAGE_2 = _Paths.PROFILEPAGE_2;
+   static const ON_BOARDING_FOR_SOCIAL = _Paths.ON_BOARDING_FOR_SOCIAL;
+  static const CREATE_ACCOUNT = _Paths.CREATE_ACCOUNT;
+  static const LOGIN = _Paths.LOGIN;
+  static const SIGN_UP = _Paths.SIGN_UP;
+  static const CODE_VERIFICATION = _Paths.CODE_VERIFICATION;
+  static const CREATE_PASSWORD = _Paths.CREATE_PASSWORD;
+  static const EMAIL_VERIFICATION = _Paths.EMAIL_VERIFICATION;
+  static const PIN_VERIFICATION = _Paths.PIN_VERIFICATION;
+  static const DASHBOARD = _Paths.DASHBOARD;
+  static const OTP_SIGNIN = _Paths.OTP;
+  static const HOME = _Paths.HOME;
+  static const INBOX = _Paths.INBOX;
+  static const CHAT = _Paths.CHAT;
+  static const GATHER_NEW_CHAT_INFO = _Paths.GATHER_NEW_CHAT_INFO;
+  static const INFO = _Paths.INFO;
+  static const VOICE_CHAT = _Paths.VOICE_CHAT;
+  static const PAYMENT_CONFIRMATION = _Paths.PAYMENT_CONFIRMATION;
+  static const PAYMENT_METHOD = _Paths.PAYMENT_METHOD;
+  static const PAYMENT_PLAN = _Paths.PAYMENT_PLAN;
+  static const CREATE_PIN = _Paths.CREATE_PIN;
+  static const PIN_SUCCESS = _Paths.PIN_SUCCESS;
+  static const PROFILE = _Paths.PROFILE;
+  static const SETTINGS = _Paths.SETTINGS;
+  static const NOTIFICATION_SETTINGS = _Paths.NOTIFICATION_SETTINGS;
+  static const SPARKD_LINES_RESPONSE = _Paths.SPARKD_LINES_RESPONSE;
+  static const SPARKD_LINES = _Paths.SPARKD_LINES;
+  static const START_SPARKD = _Paths.START_SPARKD;
+  static const TERMS_CONDITION = _Paths.TERMS_CONDITION;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const SPLASH = '/splash';
+  static const ON_BOARDING = '/on-boarding';
+  static const PROFILEPAGE_2 = '/profile_page_2';
+  static const ON_BOARDING_FOR_SOCIAL = '/on-boarding-social';
+  static const CREATE_ACCOUNT = '/create-account';
+  static const LOGIN = '/login';
+  static const SIGN_UP = '/sign-up';
+  static const CODE_VERIFICATION = '/code-verification';
+  static const CREATE_PASSWORD = '/create-password';
+  static const EMAIL_VERIFICATION = '/email-verification';
+  static const PIN_VERIFICATION = '/pin-verification';
+  static const DASHBOARD = '/dashBoard';
+  static const OTP = '/otp_Screen';
+  static const HOME = '/home';
+  static const INBOX = '/inbox';
+  static const CHAT = '/chat';
+  static const GATHER_NEW_CHAT_INFO = '/gather-new-chat-info';
+  static const INFO = '/info';
+  static const VOICE_CHAT = '/voice-chat';
+  static const PAYMENT_CONFIRMATION = '/payment-confirmation';
+  static const PAYMENT_METHOD = '/payment-method';
+  static const PAYMENT_PLAN = '/payment-plan';
+  static const CREATE_PIN = '/create-pin';
+  static const PIN_SUCCESS = '/pin-success';
+  static const PROFILE = '/profile';
+  static const SETTINGS = '/settings';
+  static const NOTIFICATION_SETTINGS = '/notification-settings';
+  static const SPARKD_LINES_RESPONSE = '/sparkd-lines-response';
+  static const SPARKD_LINES = '/sparkd-lines';
+  static const START_SPARKD = '/start-sparkd';
+  static const TERMS_CONDITION = '/terms-condition';
+}
