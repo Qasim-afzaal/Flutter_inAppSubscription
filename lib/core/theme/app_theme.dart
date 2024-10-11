aa18e9e18541f6885653bc24a8f6779699a2c499
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-12 00:01:57 +0500

    add app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,31 @@
+import 'package:sparkd/core/extensions/app_theme_extension.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppTheme {
+  MaterialColor get primarySwatch;
+
+  MaterialColor createMaterialColor(Color color);
+
+  Color get primary;
+
+  Color get onPrimary;
+
+  Color get secondary;
+
+  Color get onSecondary;
+
+  Color get errorColor;
+
+  Color get scaffoldBackgroundColor;
+
+  Color get cardColor;
+  Color get chatBubbleColor;
+
+  Color get grey;
+
+  AppThemeExtension get extension;
+
+  InputDecorationTheme get inputDecorationTheme;
+
+  TextTheme get textTheme;
+}
