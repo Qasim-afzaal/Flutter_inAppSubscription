56c2fefff0118cbe5b2dd775d22c93dc36a8f27c
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-16 01:44:26 +0500

    update name widget

--- lib/widgets/name_widget.dart
+++ lib/widgets/name_widget.dart
@@ -1,5 +1,10 @@
-import 'package:sparkd/core/constants/imports.dart';
-import 'package:sparkd/pages/new_chat/gather_new_chat/gather_new_chat_info_controller.dart';
+
+import 'package:flutter_inappsubscriptions/core/components/app_button.dart';
+import 'package:flutter_inappsubscriptions/core/components/app_text_field.dart';
+import 'package:flutter_inappsubscriptions/core/components/sb.dart';
+import 'package:flutter_inappsubscriptions/core/constants/app_strings.dart';
+import 'package:flutter_inappsubscriptions/core/constants/imports.dart';
+import 'package:flutter_inappsubscriptions/core/extensions/build_context_extension.dart';
 
 class NameWidget extends StatelessWidget {
   const NameWidget({super.key, required this.controller});
@@ -30,7 +35,7 @@ class NameWidget extends StatelessWidget {
         ),
         SB.h(25),
         AppButton.primary(
-          title: AppStrings.getMySparkd,
+          title:" AppStrings.getMySparkd",
           onPressed: controller.getSparkd,
         )
       ],
