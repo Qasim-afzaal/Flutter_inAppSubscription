4cae67058c8ee337c2c5c7011b53e488d28f9f97
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-19 02:45:48 +0500

    add get payment function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappsubscriptions/core/constants/imports.dart';
+import 'package:flutter_inappsubscriptions/payment/payment_plan/payment_plan_controller.dart';
+import 'package:flutter_inappsubscriptions/settings/inapp_purchase_source.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+PaymentPlanController getPaymentPlanController() {
+  if (Get.isRegistered<PaymentPlanController>()) {
+    debugPrint("Payment Dependencies already register");
+    return Get.find<PaymentPlanController>();
+  } else {
+    debugPrint("Payment Dependencies going to register");
+    Get.lazyPut<InAppPurchaseSource>(
+      () => InAppPurchaseSourceImpl(),
+      fenix: true,
+    );
+
+    Get.lazyPut<PaymentPlanController>(
+      () => PaymentPlanController(inAppPurchaseSource: Get.find<InAppPurchaseSource>()),
+      fenix: true,
+    );
+    return Get.find<PaymentPlanController>();
+  }
 }
 
 class MyApp extends StatefulWidget {
