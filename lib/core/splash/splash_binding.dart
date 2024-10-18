44b8da9ce07b8e2ee340b38ec7b220ee995648f9
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-19 02:46:12 +0500

    add splash binding

---
+++ lib/core/splash/splash_binding.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_inappsubscriptions/core/splash/splash_controller.dart';
+import 'package:get/get.dart';
+
+
+class SplashBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<SplashController>(
+      () => SplashController(),
+    );
+  }
+}
