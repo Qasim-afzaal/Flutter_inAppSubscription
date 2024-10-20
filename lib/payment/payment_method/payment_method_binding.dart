bee9f813201bfdd455b15cc23855f044f643d206
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-21 01:53:44 +0500

    update import

--- lib/payment/payment_method/payment_method_binding.dart
+++ lib/payment/payment_method/payment_method_binding.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_inappsubscriptions/payment/payment_method/payment_method_controller.dart';
 import 'package:get/get.dart';
-import 'package:sparkd/pages/payment/payment_method/payment_method_controller.dart';
 
 class PaymentMethodBinding extends Bindings {
   @override
