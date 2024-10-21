c724f6c0e9455435e0477ecb88aedc985d97a26d
Qasim-afzaal <qasim.afzaal432@gmail.com>
2024-10-22 01:23:36 +0500

    add personailty wigdet

--- lib/widgets/personality_widget.dart
+++ lib/widgets/personality_widget.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter_inappsubscriptions/core/components/app_button.dart';
 import 'package:flutter_inappsubscriptions/core/components/sb.dart';
 import 'package:flutter_inappsubscriptions/core/constants/app_strings.dart';
 import 'package:flutter_inappsubscriptions/core/constants/imports.dart';
@@ -22,23 +22,27 @@ class PersonalityWidget extends StatelessWidget {
   final List<PersonalityModel> _personalityList = [
     PersonalityModel(
       personality: Personality.Seductive,
-      image: Assets.icons.seductuve,
-      description: "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
+      image: AssetImage("assetName"),
+      description:
+          "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
     ),
     PersonalityModel(
       personality: Personality.Extrovert,
-      image: Assets.icons.extrovert,
-      description: "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
+      image: AssetImage("assetName"),
+      description:
+          "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
     ),
     PersonalityModel(
       personality: Personality.Introvert,
-      image: Assets.icons.introvert,
-      description: "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
+      image: AssetImage("assetName"),
+      description:
+          "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
     ),
     PersonalityModel(
       personality: Personality.Romantic,
-      image: Assets.icons.romantic,
-      description: "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
+      image: AssetImage("assetName"),
+      description:
+          "Lorem ipsum dolor sit amet. Qui nobisnostrum ut voluptas incidunt.",
     ),
   ];
 
@@ -82,7 +86,10 @@ class PersonalityWidget extends StatelessWidget {
 }
 
 class _Container extends StatelessWidget {
-  const _Container({required this.data, this.selectedPersonality, required this.onPersonalitySelection});
+  const _Container(
+      {required this.data,
+      this.selectedPersonality,
+      required this.onPersonalitySelection});
 
   final PersonalityModel data;
   final Personality? selectedPersonality;
@@ -96,31 +103,42 @@ class _Container extends StatelessWidget {
       padding: const EdgeInsets.all(1),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(24),
-        gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
-          context.primary,
-          context.secondary,
-        ]),
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              context.primary,
+              context.secondary,
+            ]),
       ),
       child: InkWell(
-        onTap: () { 
-           onPersonalitySelection(data.personality);},
+        onTap: () {
+          onPersonalitySelection(data.personality);
+        },
         child: Container(
           padding: const EdgeInsets.all(15),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(24),
-            color: data.personality == selectedPersonality ? null : context.scaffoldBackgroundColor,
+            color: data.personality == selectedPersonality
+                ? null
+                : context.scaffoldBackgroundColor,
             gradient: data.personality == selectedPersonality
-                ? LinearGradient(begin: Alignment.centerLeft, end: Alignment.centerRight, colors: [
-                    context.secondary,
-                    context.primary,
-                  ])
+                ? LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                        context.secondary,
+                        context.primary,
+                      ])
                 : null,
           ),
           child: Stack(
             children: [
-              data.image.svg(
-                color: data.personality == selectedPersonality ? Colors.white : context.primary,
-              ),
+              // data.image.svg(
+              //   color: data.personality == selectedPersonality
+              //       ? Colors.white
+              //       : context.primary,
+              // ),
               Positioned(
                 top: 0,
                 bottom: 0,
@@ -131,7 +149,9 @@ class _Container extends StatelessWidget {
                     data.personality.name,
                     style: context.titleLarge?.copyWith(
                       fontWeight: FontWeight.w500,
-                      color: data.personality == selectedPersonality ? Colors.white : null,
+                      color: data.personality == selectedPersonality
+                          ? Colors.white
+                          : null,
                     ),
                   ),
                 ),
@@ -164,8 +184,11 @@ class _Container extends StatelessWidget {
 
 class PersonalityModel {
   final Personality personality;
-  final SvgGenImage image;
+  final AssetImage image;
   final String description;
 
-  PersonalityModel({required this.personality, required this.image, required this.description});
+  PersonalityModel(
+      {required this.personality,
+      required this.image,
+      required this.description});
 }
